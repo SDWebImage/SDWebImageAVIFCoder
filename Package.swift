ad88f8150339c96cbf5ccd595fb913133d1aee5c
DreamPiggy <lizhuoli1126@126.com>
2023-05-16 15:23:52 +0800

    Fix the compile issue with libavif 0.11.0+ by removing the limit alpha range check
    Update the min deps to 0.11.0+ as well

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.10.0"),
-        .package(url: "https://github.com/SDWebImage/libavif-Xcode.git", from: "0.9.1")
+        .package(url: "https://github.com/SDWebImage/libavif-Xcode.git", from: "0.11.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
